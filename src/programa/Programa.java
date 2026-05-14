f8526a0cfdf995fe1c7307e857e0523a3427bec8
25dawbrian <25.daw.brianarias@colegiomontessori.com>
2026-05-14 19:23:03 +0200

    nuevo

--- src/programa/Programa.java
+++ src/programa/Programa.java
@@ -69,7 +69,6 @@ public class Programa {
 				gestor.eliminarOrdenadorPorEtiqueta();
 				break;
 			default:
-				//actualizacion git
 				System.out.println("Opcion no contemplada");
 				break;
 			}
